93dd8dfbb0f473d2bdf45128559419b006ce0ba1
Minanshu Singh <minanshu.1998@gmail.com>
2019-01-28 20:10:26 +0530

    remove concurrently, fixed buggy dev launch

--- scripts/chrome-launch.js
+++ scripts/chrome-launch.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
-const chromeLaunch = require('chrome-launch'); // eslint-disable-line import/no-extraneous-dependencies
 const path = require('path');
+const chromeLaunch = require('chrome-launch'); // eslint-disable-line import/no-extraneous-dependencies
+
+require('colors');
 
 const url = 'https://google.com';
 const dev = path.resolve(__dirname, '..', 'dev');
 const args = [`--load-extension=${dev}`];
 
 chromeLaunch(url, { args });
-console.log('A new instance of Chrome should now be open in the background.'); // eslint-disable-line no-console
+console.log('A new instance of Chrome will open in the background.'.blue.bold); // eslint-disable-line no-console
--- scripts/dev.js
+++ scripts/dev.js
@@ -3,7 +3,13 @@
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
+const execa = require('execa');
 const webpack = require('webpack');
+const argv = require('yargs').argv;
+
+require('colors');
+
+let done = false;
 
 const webpackConfigFactory = require('../config/webpack/webpack.config');
 
@@ -19,5 +25,15 @@ compiler.watch({
 }, (err, stats) => {
   // Print watch/build result here...
   console.log(stats);
+  if (argv.browser === 'chrome' && !done) {
+    done = true;
+    execa('node', ['scripts/chrome-launch.js']).stdout.pipe(process.stdout);
+  }
+  else if (argv.browser === 'firefox') {
+    done = true;
+    console.log('A new instance of Firefox will open in the background.'.blue.bold);
+    execa('web-ext', ['run', '--source-dir', 'dev', '--pref', 'startup.homepage_welcome_url=https://www.youtube.com']);
+  }
+
 });
 
